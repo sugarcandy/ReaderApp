16d546c563cb4d405a86d983e9a7c287943fb69a
Sugarcandy <1947291437@qq.com>
2019-06-26 14:18:31 +0800

    change background

--- app/src/main/java/hw/txtreader/MApplication.java
+++ app/src/main/java/hw/txtreader/MApplication.java
@@ -1,4 +1,3 @@
-//Copyright (c) 2017. 章钦豪. All rights reserved.
 package hw.txtreader;
 
 import android.app.Application;
@@ -10,21 +9,7 @@ public class MApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-     /*   if (BuildConfig.IS_RELEASE) {
-            String channel = "debug";
-            try {
-                ApplicationInfo appInfo = getPackageManager()
-                        .getApplicationInfo(getPackageName(),
-                                PackageManager.GET_META_DATA);
-                channel = appInfo.metaData.getString("UMENG_CHANNEL_VALUE");
-            } catch (PackageManager.NameNotFoundException e) {
-                e.printStackTrace();
-            }
-            MobclickAgent.startWithConfigure(new MobclickAgent.UMAnalyticsConfig(this, getString(R.string.umeng_key), channel, MobclickAgent.EScenarioType.E_UM_NORMAL, true));
-        }*/
         instance = this;
-//        ProxyManager.initProxy();
-//        startService(new Intent(this, DownloadService.class));
     }
 
     public static MApplication getInstance() {
